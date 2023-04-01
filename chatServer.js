4652a6fb7644f20eada731d49590d9825f0692a1
Phillip-Lisun <l.phillip@wustl.edu>
2023-04-01 18:09:09 -0500

    css

--- chatServer.js
+++ chatServer.js
@@ -1,36 +1,13 @@
 // Require the packages we will use:
-const http = require("http"),
-    fs = require("fs");
-
 const port = 3456;
-const html = "chatClient.html";
-const css = "client.css";
 
-//copied code start : https://stackoverflow.com/questions/24582338/how-can-i-include-css-files-using-node-express-and-ejs
+const express = require('express'),
+     http = require('http');
+const app = express();
 
-function onRequest(request, response){  
-    if(request.headers.accept.split(',')[0] == 'text/css') {
+const server = http.createServer(app);
 
-         fs.readFile(css, (err, data)=>{
-             response.writeHeader(200, {'Content-Type': 'text/css'});
-             response.write(data);
-             response.end();
-         });  
-    }
-    else {
-
-        fs.readFile(html, function(err, data){
-            response.writeHead(200, {'Content_type': 'text/html'});
-            response.write(data);
-            response.end();
-        });
-    };
-};
-
-const server = http.createServer(onRequest);
-//copied code end
-
-server.listen(port);
+app.use(express.static('public'));
 
 // Import Socket.IO and pass our HTTP server object to it.
 const socketio = require("socket.io")(http, {
@@ -49,3 +26,5 @@ io.sockets.on("connection", function (socket) {
         io.sockets.emit("message_to_client", { message: data["message"] }) // broadcast the message to other users
     });
 });
+
+server.listen(port);
