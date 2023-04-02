bc5f0bba675f4c9b7008daff9beb3bfd07ce4eea
Phillip-Lisun <l.phillip@wustl.edu>
2023-04-02 18:06:55 -0500

    js

--- chatServer.js
+++ chatServer.js
@@ -1,6 +1,10 @@
 // Require the packages we will use:
 const port = 3456;
 
+let roomIdGen = 1;
+
+let publicRooms = new Map();
+
 const express = require('express'),
      http = require('http');
 const app = express();
@@ -25,6 +29,39 @@ io.sockets.on("connection", function (socket) {
         console.log("message: " + data["message"]); // log it to the Node.JS output
         io.sockets.emit("message_to_client", { message: data["message"] }) // broadcast the message to other users
     });
+
+    socket.on('create_room_request', function(data) {
+
+        if(data.createRoomPassword == "") {
+
+            let newRoomId = roomIdGen + "";
+            roomIdGen++;
+
+            publicRooms.set(newRoomId, data.createRoomName);
+
+            socket.join(newRoomId);
+
+
+            console.log("Room Joined: " + newRoomId);
+            console.log(publicRooms);
+            
+
+
+            io.sockets.to(newRoomId).emit('roomMessage', {roomId: newRoomId, message: "Welcome!"});
+            io.sockets.
+
+        }        
+
+
+        console.log(data.createRoomName + " " + data.createRoomPassword);
+
+    });
+
+
+
+
+
+
 });
 
 server.listen(port);
--- clientJS.js
+++ clientJS.js
@@ -1,1 +1,66 @@
-// alert("Hello test1 from seank!");
+const socket = io();
+
+
+let username;
+
+var socketio = io.connect();
+
+
+
+
+Window.onload = onLoadFunction();
+
+function onLoadFunction() {
+
+    document.getElementById("mainArea").style.display = 'none';
+    document.getElementById("setUsername").style.display= 'block';
+
+    document.getElementById("usernameButton").addEventListener("click", setUsername, false);
+   
+}
+
+function setUsername() {
+
+    usernameEntry = document.getElementById("username").value + "";
+
+    if(usernameEntry == "") {
+        alert("Username Required");
+    }
+    else {
+        username = usernameEntry;
+
+
+        document.getElementById("mainArea").style.display = 'flex';
+        document.getElementById("setUsername").style.display= 'none';
+
+        setEventListeners();
+
+
+    }
+}
+
+
+//Event listeners to join a room or create a room
+function setEventListeners() {
+
+    document.getElementById("createRoomButton").addEventListener('click', createRoom, false);
+
+    //add join room eventListeners
+
+
+}
+
+function createRoom() {
+
+    let createRoomName = document.getElementById("roomNameText").value + "";
+    let createRoomPassword = document.getElementById("roomPasswordText").value + "";
+
+    socketio.emit("create_room_request", {createRoomName: createRoomName, createRoomPassword: createRoomPassword});
+
+    alert(createRoomName + " " + createRoomPassword);
+}
+
+socketio.on('roomMessage', (data) => {
+    alert(data.roomId + " " + data.message);
+
+});
